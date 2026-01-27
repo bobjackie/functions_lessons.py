3a0c75254640df8cb5bf3fecf4ce3e39b2cee557
bobjackie <jackie.aug31@gmail.com>
2026-01-27 20:28:57 +0000

    py

--- 1_.py
+++ 1_.py
@@ -1,6 +1,7 @@
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
+
 # ,
 
 # :
@@ -31,4 +32,60 @@
 # phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
 # tv_brands = {"Sony", "Philips", "Samsung", "LG"}
 # Search the documentation for the requested method to know how it works.
+
+
+# function = A block of reusable code 
+#            place () after the function name to invoke it 
+
+# def happy_birthday(name, age):
+#     print(f"Happy birthday to {name}!")
+#     print(f"You are {age} years old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# happy_birthday("Bro", 20) 
+# happy_birthday("Steve", 30) 
+# happy_birthday("Joe ", 40) 
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"your bill of ${amount:. 2f}is due {due_date}")
+
+# display_invoice("BroCode", 42.50, "01/01")
+# display_invoice("Marco", 39.50, "01/02")
+
+# # return = statement used to end a function 
+# #           and send a result back to the caller
+
+# def add (x, y): 
+#     z = x + y 
+#     return z 
+
+# def subtract(x, y): 
+#     z = x - y 
+#     return z 
+
+# def multiply(x, y):
+#     z = x * y 
+#     return z 
+
+# def divide(x, y ):
+#     z = x / y 
+#     return z 
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+
+
+# def create_name(first, last ): 
+#     first = first.capitalize() 
+#     last = last.capitalize()
+#     return first +""+ last
+
+# full_name = create_name("spongebob", "squarepants")
+
+# print(full_name)
 
