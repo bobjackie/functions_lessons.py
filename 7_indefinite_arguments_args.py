8327536099c214f3e17762b78dce042bc005470f
bobjackie <jackie.aug31@gmail.com>
2026-01-29 20:43:27 +0000

    describe

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,12 +1,45 @@
+# def tea_order(customer_name, tea_type, **kwargs):
+#     # print(customer_name, "ordered a", tea_type, "tea")
+#     # for key, value in kwargs.items():
+#     #     print(" - Add", key, ":", value)
+#     for arg in args:
+#         print(" - Add", arg)
+#     for key, value in kwargs.items():
+#         print("  -Add", key, ":", value)
+
+# # tea_order("Alice", "chamonille")
+# # tea_order("Bob", "black", milk="oat")
+# tea_order("Tony","black", milk="oat", sweetener="honey")
+
+
+
+
+
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
+def sum_squares(*args):
+    sum = 0 
+    for num in args:
+        sum += num ** 2
+    return sum
 
 
+print(sum_squares(1,2,3))
 # Indefinite Arguments (*args) Practice #2
 # Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
 
+
+
+def absolutesum(*args):
+    total = 0
+    for num in args:
+        total += abs(num)
+    return total
+print(absolutesum(-1, -2, 3))
 # Indefinite Arguments (*args) Practice #3
 # Create a function called personal_numbers that receives, as its first argument, a name, and then an indefinite number of values.
 
